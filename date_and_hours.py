8f0cea8a0f80adb72bbbac4ab6e67a14824e5bef
KevinBacon <turnipsh@student.21-school.ru>
2023-10-05 13:46:46 +0300

    time_hours

--- date_and_hours.py
+++ date_and_hours.py
@@ -2,21 +2,26 @@ import datetime
 
 import data
 
+from zoneinfo import ZoneInfo
+from datetime import datetime
+
+time_zone = ZoneInfo('Europe/Moscow')
+
 
 def plus_day_to_current_time(day):
     return int((datetime.timedelta(days=day) + get_current_datetime()).timestamp())
 
 
 def get_current_hour():
-    return datetime.datetime.now().hour
+    return get_current_datetime().hour
 
 
 def get_current_week():
-    return datetime.datetime.now().weekday()
+    return get_current_datetime().weekday()
 
 
 def get_current_datetime():
-    now = datetime.datetime.now().replace(second=0, microsecond=0, minute=0)
+    now = datetime.datetime.now(time_zone).replace(second=0, microsecond=0, minute=0)
     return now
 
 
