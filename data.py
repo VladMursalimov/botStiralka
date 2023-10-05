a368d2f5762de1844324b3a85aff3779ec858d32
KevinBacon <turnipsh@student.21-school.ru>
2023-10-05 16:51:00 +0300

    add 3 hour delta

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
-times_hours = [i for i in range(8, 24, 3)]
+times_hours = [i for i in range(8, 24, 3) if i + 3 <= 24]
 
-times = [f'с {i}:00 до {i + 3}:00' for i in times_hours]
+times = [f'с {"00" if i == 24 else i}:00 до {"00" if i + 3 == 24 else i + 3}:00' for i in times_hours]
 
 day_deltas = [
     "Сегодня",
