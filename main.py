43b3813ef2f81eaa2e86efd53cb2cbfe818b9fe8
KevinBacon <turnipsh@student.21-school.ru>
2023-10-06 09:22:10 +0300

    fix bug

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ async def main() -> None:
     from aiogram.client.session.aiohttp import AiohttpSession
     session = AiohttpSession(proxy="http://proxy.server:3128")
 
-    bot = Bot(TOKEN, session=None, parse_mode=ParseMode.HTML)
+    bot = Bot(TOKEN, session=session, parse_mode=ParseMode.HTML)
     await sqlite_db.clean_time()
     await dp.start_polling(bot)
     await sqlite_db.db_connect()
